9206c02c9aafd44bacbf0027146374e8b1ca0f33
Tan Woon Kin <woonkin14@gmail.com>
2025-09-07 23:51:43 +0800

    advantages page centering, deployment preparation

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+  },
+  build: {
+    outDir: 'dist',
+  },
 })
